6d2ccc1ad06fb5316b9bf399a465171ac6d8537d
Patrick Teulings <patrickteulings@gmail.com>
2019-12-18 18:07:29 +0100

    Hamburger WIP

--- src/components/navigation/Navigation.vue
+++ src/components/navigation/Navigation.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="navigation">
     <div class="navigation__bar">
+      <div class="navigation__trigger-wrapper">
+        <div class="navigation__trigger is-open">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
       <nav>
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
